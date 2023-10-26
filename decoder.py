0b35fb899a409c9bea5ed3cc3a3f2c6f853ebe93
Artem-Antonian <149022931+Artem-Antonian@users.noreply.github.com>
2023-10-25 21:10:05 -0400

    decoder.py

---
+++ decoder.py
@@ -0,0 +1,13 @@
+# Artem Antonian
+def decode(to_decode):
+    new_password = ""
+    for i in to_decode:
+        if int(i) > 2:
+            decoded_num = str(int(i) - 3)
+        elif int(i) == 0:
+            decoded_num = "7"
+        elif int(i) == 1:
+            decoded_num = "8"
+        elif int(i) == 2:
+            decoded_num = "9"
+        new_password += decoded_num
